e12dcbd3ad981faf36b1c5d59f2e40a751919c3e
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-06-25 14:49:03 -0300

    Styled-components

---
+++ src/screens/NewComp/index.tsx
@@ -0,0 +1,12 @@
+
+import { Container, Title } from './styles';
+
+export default function NewComp() {
+  return (
+    <Container>
+    <Title>
+      COMP
+    </Title>
+  </Container>
+  );
+}
---
+++ src/screens/NewComp/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 0.2;
+  background-color: #DF1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #FFF;
+  font-size: 32px;
+`;
