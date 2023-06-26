46c8b3f17e2e893c69fd09fdde777932f91bb02b
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-06-26 08:26:00 -0300

    Criação de fonte do Google e  faz o teste se a fonte foi carregada se não mostra o activityIndicator

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,26 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Containerdois = styled.View`
-
-  flex: 1;
-  background-color: #CCFF;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: row;
-  gap: 56;
- 
-`;
-
-export const Title = styled.Text`
    background-color: ${({ theme }) => theme.COLORS.RED};
   padding: 24px;
-  
 `;
